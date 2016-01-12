fac4def41b71587aded8e31b6662ee4e3d466a81
ALEX SLYE <slyealex@live.com>
2016-01-12 17:20:24 -0500

    changed where Bitmaker was in exercise4

--- exercise4.rb
+++ exercise4.rb
@@ -1,10 +1,10 @@
 (1..100).each do |num|
   if num % 5 == 0 && num % 3 == 0
-    puts "Bit"
+    puts "Bitmaker"
   elsif num % 5 == 0
     puts "Maker"
   elsif num % 3 == 0
-    puts "Bitmaker"
+    puts "Bit"
   else
     puts num
   end

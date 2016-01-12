1c4cd8f7b5f8e5ce89f8fc3f6a14b447f5fbb1c2
ALEX SLYE <slyealex@live.com>
2016-01-12 14:04:39 -0500

    completed exercise2

---
+++ exercise2.rb
@@ -0,0 +1,20 @@
+#EXERCISE 2
+
+
+#####  QUESTION ONE #######
+meal_cost = 55.0
+tip = 0.18
+puts "The tip for a #{meal_cost} dollar meal is #{ 55 * 18 / 100 } buckaroos."
+
+#####  QUESTION TWO #######
+puts "The Bitmaker course is  " + 9.to_s + " weeks."
+
+
+#####  QUESTION THREE #######
+puts "The tallest man I ever saw was #{45628 * 7839} milimeters tall."
+
+
+#####  QUESTION FOUR #######
+(10 < 20 && 30 < 20) || !(10 == 11)
+
+#My guess of true was correct!

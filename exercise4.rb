3d9a5742101824a1154e25c136dc4818ce46d374
ALEX SLYE <slyealex@live.com>
2016-01-12 15:07:17 -0500

    finished exercise4 with some help

--- exercise4.rb
+++ exercise4.rb
@@ -1,9 +1,9 @@
-100.times do |num|
-  if num % 3 == 0
+(1..100).each do |num|
+  if num % 5 == 0 && num % 3 == 0
     puts "Bit"
   elsif num % 5 == 0
     puts "Maker"
-  elsif num % (3 && 5) == 0
+  elsif num % 3 == 0
     puts "Bitmaker"
   else
     puts num

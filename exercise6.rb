c90b7e99ecfdd9b5be254d30d89a730fba196051
ALEX SLYE <slyealex@live.com>
2016-01-13 15:07:31 -0500

    finished exercise6 with some clarification from fred

--- exercise6.rb
+++ exercise6.rb
@@ -1,4 +1,4 @@
-grocery_list = ["carrots", "toilet paper", "cookies", "fish"]
+grocery_list = ["carrots", "toilet paper", "cookies", "fish", "rice"]
 
 # grocery_list.add_item("rice")
 
@@ -14,8 +14,21 @@ end
 
 output_list(grocery_list)
 
-# def grocery_list(item)
-#   grocery_list = [] << item
-# end
-#
-# grocery_list("rice")
+puts grocery_list.length
+
+
+if grocery_list.include?("bananas") == false
+  puts "You need to pick up some bananas!"
+else
+    puts "You don't need to pick up banans today."
+end
+
+puts "the second item on my list is #{grocery_list[1]}"
+
+grocery_list.sort
+output_list(grocery_list)
+#line break
+puts ""
+#I used fish instead of salmon
+grocery_list.delete("fish")
+output_list(grocery_list)
